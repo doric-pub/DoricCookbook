936c8e52cdf459c9727cfb5126e1a2d5c1d8be4f
吴尚昆 <shangkunwu@msn.com>
2022-02-21 18:04:53 +0800

    Android support Dangle.【3D模型加载】

--- android/app/src/main/java/pub/doric/android/MainApplication.java
+++ android/app/src/main/java/pub/doric/android/MainApplication.java
@@ -3,11 +3,20 @@ package pub.doric.android;
 import android.app.Application;
 
 import pub.doric.Doric;
+import pub.doric.devkit.DoricDev;
+
+import pub.doric.dangle.library.DangleLibrary;
+import pub.doric.library.three.DoricThreeLibrary;
+
 
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Doric.init(this);
+        DoricDev.getInstance();
+
+        Doric.registerLibrary(new DangleLibrary());
+        Doric.registerLibrary(new DoricThreeLibrary());
     }
 }
